c59468ad7b11267552f1daae35b39433b94e5196
robin04061 <59394908+robin04061@users.noreply.github.com>
2023-01-30 11:54:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import streamlit as st
 #Text elements
-st.title("한국투자증권 신입사원 **:red[DT]** 프로젝트:)
+st.title("한국투자증권 신입사원")
 st.header("Streamlit 프로토타입 만들기")
 st.subheader("[Streamlit] 활용하기")
-st.markdown("*** kim yong wook web application '***\n\n)
+st.markdown("ffff")
